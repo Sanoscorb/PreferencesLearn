eff3e93c1725ae4a3ca77b8df3095fba606292fa
Sanoscorb <sanoscorb@gmail.com>
2025-10-09 16:59:13 +0700

    Names refactor

--- app/src/main/java/ru/sanoscorb/preferenceslearn/MainActivity.java
+++ app/src/main/java/ru/sanoscorb/preferenceslearn/MainActivity.java
@@ -11,9 +11,9 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends Activity implements OnClickListener {
-    EditText etText;
-    Button btnSave, btnLoad;
-    SharedPreferences sPref;
+    EditText editText;
+    Button saveButton, loadButton;
+    SharedPreferences preferences;
     final String SAVED_TEXT = "saved_text";
 
     @Override
@@ -21,33 +21,33 @@ public class MainActivity extends Activity implements OnClickListener {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        etText = findViewById(R.id.etText);
-        btnSave = findViewById(R.id.btnSave);
-        btnSave.setOnClickListener(this);
-        btnLoad = findViewById(R.id.btnLoad);
-        btnLoad.setOnClickListener(this);
+        editText = findViewById(R.id.edit_text);
+        saveButton = findViewById(R.id.button_save);
+        saveButton.setOnClickListener(this);
+        loadButton = findViewById(R.id.button_load);
+        loadButton.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
-        if (v.getId() == R.id.btnSave)
+        if (v.getId() == R.id.button_save)
             saveText();
-        else if (v.getId() == R.id.btnLoad)
+        else if (v.getId() == R.id.button_load)
             loadText();
     }
 
     void saveText() {
-        sPref = getPreferences(MODE_PRIVATE);
-        Editor ed = sPref.edit();
-        ed.putString(SAVED_TEXT, etText.getText().toString());
+        preferences = getPreferences(MODE_PRIVATE);
+        Editor ed = preferences.edit();
+        ed.putString(SAVED_TEXT, editText.getText().toString());
         ed.commit();
         Toast.makeText(this, R.string.toastSave, Toast.LENGTH_SHORT).show();
     }
 
     void loadText() {
-        sPref = getPreferences(MODE_PRIVATE);
-        String savedText = sPref.getString(SAVED_TEXT, "");
-        etText.setText(savedText);
+        preferences = getPreferences(MODE_PRIVATE);
+        String savedText = preferences.getString(SAVED_TEXT, "");
+        editText.setText(savedText);
         Toast.makeText(this, R.string.toastLoad, Toast.LENGTH_SHORT).show();
     }
 }
