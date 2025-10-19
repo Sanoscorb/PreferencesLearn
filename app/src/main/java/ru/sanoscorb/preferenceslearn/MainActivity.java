eeb1711be9283f5408a14bb3fd02f0483640befb
Sanoscorb <sanoscorb@gmail.com>
2025-10-19 15:49:10 +0700

    Theme changed to AppCompat

--- app/src/main/java/ru/sanoscorb/preferenceslearn/MainActivity.java
+++ app/src/main/java/ru/sanoscorb/preferenceslearn/MainActivity.java
@@ -1,6 +1,5 @@
 package ru.sanoscorb.preferenceslearn;
 
-import android.app.Activity;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
@@ -10,7 +9,13 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-public class MainActivity extends Activity implements OnClickListener {
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity implements OnClickListener {
     EditText editText;
     Button saveButton, loadButton;
     SharedPreferences preferences;
@@ -19,7 +24,13 @@ public class MainActivity extends Activity implements OnClickListener {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
 
         editText = findViewById(R.id.edit_text);
         saveButton = findViewById(R.id.button_save);
